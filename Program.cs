00b82edcc4bef8e82cfde9e16aea9c73f9b20c76
Ryan  Soulard <soulard1ra@gmail.com>
2023-07-13 01:11:37 -0400

    Update main file with basic demonstration

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChipSecuritySystem
 {
@@ -10,6 +7,42 @@ namespace ChipSecuritySystem
     {
         static void Main(string[] args)
         {
+            var colorChips = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Yellow, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple)
+            };
+            var chipGraph = new ChipGraph(colorChips);
+
+            var result = chipGraph.FindLongestChainBetween(Color.Blue, Color.Green);
+
+            PrintResults(result);
+
+            Console.WriteLine("Press [Enter] to close.");
+            Console.ReadLine();
+        }
+
+        static void PrintResults(IList<ColorChip> results)
+        {
+            if (results is null || results.Count == 0)
+            {
+                Console.WriteLine(Constants.ErrorMessage);
+                return;
+            }
+
+            foreach(var colorChip in results)
+            {
+                Console.Write(colorChip.ToString());
+
+                if (results.IndexOf(colorChip) != results.Count - 1)
+                {
+                    Console.Write(" -> ");
+                }
+            }
+
+            Console.WriteLine();
         }
     }
 }
